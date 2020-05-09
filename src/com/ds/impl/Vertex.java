94c4171f2016c14ed3529a20014621c14f2ecde3
Kedar Patil <knpatil512@gmail.com>
2020-05-10 00:39:23 +0530

    WIP changes for bit masking and DP problem

--- src/com/ds/impl/Vertex.java
+++ src/com/ds/impl/Vertex.java
@@ -1,5 +1,8 @@
 package com.ds.impl;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Vertex {
 	private String name;
 	private boolean isVisited;
@@ -8,16 +11,20 @@ public class Vertex {
 	private Vertex parent;
 	private int edge;
 	private int mainVertexListIndex;
+	private Set<Integer> dataList;
+	private int maskedBits;
 	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + arrayIndex;
+		result = prime * result + ((dataList == null) ? 0 : dataList.hashCode());
 		result = prime * result + distance;
 		result = prime * result + edge;
 		result = prime * result + (isVisited ? 1231 : 1237);
 		result = prime * result + mainVertexListIndex;
+		result = prime * result + maskedBits;
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
 		return result;
@@ -33,6 +40,11 @@ public class Vertex {
 		Vertex other = (Vertex) obj;
 		if (arrayIndex != other.arrayIndex)
 			return false;
+		if (dataList == null) {
+			if (other.dataList != null)
+				return false;
+		} else if (!dataList.equals(other.dataList))
+			return false;
 		if (distance != other.distance)
 			return false;
 		if (edge != other.edge)
@@ -40,6 +52,8 @@ public class Vertex {
 		if (isVisited != other.isVisited)
 			return false;
 		if (mainVertexListIndex != other.mainVertexListIndex)
+			return false;
+		if (maskedBits != other.maskedBits)
 			return false;
 		if (name == null) {
 			if (other.name != null)
@@ -52,6 +66,22 @@ public class Vertex {
 		} else if (!parent.equals(other.parent))
 			return false;
 		return true;
+	}
+	public int getMaskedBits() {
+		return maskedBits;
+	}
+	public void setMaskedBits(int maskedBits) {
+		this.maskedBits = maskedBits;
+	}
+	public Set<Integer> getDataList() {
+		if (null == dataList) {
+			dataList = new HashSet<>(); 
+			return dataList;
+		}
+		return dataList;
+	}
+	public void setDataList(Set<Integer> dataList) {
+		this.dataList = dataList;
 	}
 	public int getMainVertexListIndex() {
 		return mainVertexListIndex;
--- src/com/practice/DsAlgoPractice.java
+++ src/com/practice/DsAlgoPractice.java
@@ -1,99 +1,260 @@
 package com.practice;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
-import java.util.Stack;
 
+import com.ds.hackerrank.FastReader;
 import com.ds.impl.MyLinkedListGraph;
+import com.ds.impl.MySet;
 import com.ds.impl.Vertex;
 
-class Node {
+class DisjointSet {
+	List<MySet<Integer>> sets;
 
-	Node(int data) {
-		this.data = data;
+	public List<MySet<Integer>> getSets() {
+		return sets;
 	}
 
-	int data;
-	int depth;
-	Node left;
-	Node right;
+	public void setSets(List<MySet<Integer>> sets) {
+		this.sets = sets;
+	}
+	
+	public boolean makeSet(List<Integer> elements) {
+		if (null == sets) {
+			sets = new ArrayList<>();
+		}
+		
+		for (Integer e: elements) {
+			List<Integer> elemList = new ArrayList<>();
+			elemList.add(e);
+			
+			MySet<Integer> set = new MySet<>();
+			
+			set.setElements(elemList);
+			sets.add(set);
+		}
+		return true;
+	}
+	
+	public boolean union(String firstSet, String secondSet) {
+		int firstSetIndex = Integer.parseInt(firstSet);
+		int secondSetIndex = Integer.parseInt(secondSet);
+		
+		MySet<Integer> set1 = sets.get(firstSetIndex - 1);
+		MySet<Integer> set2 = sets.get(secondSetIndex - 1);
+		
+		while (null != set1.getMovedToSet()) {
+			set1 = set1.getMovedToSet();
+		}
+		
+		while (null != set2.getMovedToSet()) {
+			set2 = set2.getMovedToSet();
+		}
+		
+		if (set1.getElements().size() >= set2.getElements().size()) {
+			for (Integer e : set2.getElements()) {
+				if (!set1.getElements().contains(e)) {
+					set1.getElements().add(e);
+				}
+			}
+			set2.setMovedToSet(set1);
+		} else {
+			for (Integer e : set1.getElements()) {
+				if (!set2.getElements().contains(e)) {
+					set2.getElements().add(e);
+				}
+			}
+			set1.setMovedToSet(set2);
+		}
+		return true;
+	}
+	
+	public MySet<Integer> findSet(String element) {
+		
+		MySet<Integer> set = sets.get(Integer.parseInt(element) - 1);
+		
+		while (null != set.getMovedToSet()) {
+			set = set.getMovedToSet();
+		}
+		return set;
+	}
+}
+
+class MyCount {
+	private int count;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+}
+
+class PathDetails {
+	private int time;
+	private Set<Integer> fishTypes;
+	private int destNode;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + destNode;
+		result = prime * result + ((fishTypes == null) ? 0 : fishTypes.hashCode());
+		result = prime * result + time;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PathDetails other = (PathDetails) obj;
+		if (destNode != other.destNode)
+			return false;
+		if (fishTypes == null) {
+			if (other.fishTypes != null)
+				return false;
+		} else if (!fishTypes.equals(other.fishTypes))
+			return false;
+		if (time != other.time)
+			return false;
+		return true;
+	}
+	public int getDestNode() {
+		return destNode;
+	}
+	public void setDestNode(int destNode) {
+		this.destNode = destNode;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public Set<Integer> getFishTypes() {
+		if (null == fishTypes) {
+			fishTypes = new HashSet<>();
+		}
+		return fishTypes;
+	}
+	public void setFishTypes(Set<Integer> fishTypes) {
+		this.fishTypes = fishTypes;
+	}
 }
 
 public class DsAlgoPractice {
 
 	public static void main(String[] args) throws IOException {
 
-		String file = "src/input.txt";
-		int count = 0;
-		String line;
-		int[] a = null;
-		int q = 0;
+		String filePath = "src/input.txt";
 		boolean isCompleted = false;
-
-		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+		File file = new File(filePath);
+		
+		
+		try (InputStream in = new FileInputStream(file)) {
+			FastReader reader = new FastReader(in);
 			while (!isCompleted) {
-				q = Integer.parseInt(reader.readLine());
-				for (int qItr = 0; qItr < q; qItr++) {
-					String[] nmC_libC_road = reader.readLine().split(" ");
+				int n = reader.nextInt();
 
-					int n = Integer.parseInt(nmC_libC_road[0]);
+		        int m = reader.nextInt();
 
-					int m = Integer.parseInt(nmC_libC_road[1]);
+		        int k = reader.nextInt();
 
-					int c_lib = Integer.parseInt(nmC_libC_road[2]);
+		        List<List<Integer>> centers = new ArrayList<>();
 
-					int c_road = Integer.parseInt(nmC_libC_road[3]);
+		        for (int i = 0; i < n; i++) {
+		            List<Integer> fishTypeList = new ArrayList<>();
+		            int fishTypes = reader.nextInt();
+		            if (fishTypes > 0) {
+		                for (int j = 0; j < fishTypes; j++) {
+		                    fishTypeList.add(reader.nextInt());
+		                }
+		            }
+		            centers.add(fishTypeList);
+		        }
+		        
+		        List<List<Integer>> roads = new ArrayList<>();
 
-					int[][] cities = new int[m][2];
+		        for (int i = 0; i < m; i++) {
+		            List<Integer> connList = new ArrayList<>();
 
-					for (int i = 0; i < m; i++) {
-						String[] citiesRowItems = reader.readLine().split(" ");
+		            for (int j = 0; j < 3; j++) {
+		                connList.add(reader.nextInt());
+		            }
+		            roads.add(connList);
+		        }
 
-						for (int j = 0; j < 2; j++) {
-							int citiesItem = Integer.parseInt(citiesRowItems[j]);
-							cities[i][j] = citiesItem;
-						}
-					}
+		        int res = shop(n, k, centers, roads);
 
-					long result = roadsAndLibraries(n, c_lib, c_road, cities);
-					System.out.println(result);
-				}
+		        System.out.println(res);
 				isCompleted = true;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		 
 
+		
 		/*
-		 * int[][] cities = new int[2][2];
+		 * try (BufferedReader bufferedReader = new BufferedReader(new
+		 * FileReader(file))) { while (!isCompleted) { String[] firstMultipleInput =
+		 * bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 		 * 
-		 * cities[0][0] = 8; cities[0][1] = 2; cities[1][0] = 2; cities[1][1] = 9;
+		 * int n = Integer.parseInt(firstMultipleInput[0]);
 		 * 
-		 * cities[2][0] = 2; cities[2][1] = 4; cities[3][0] = 1; cities[3][1] = 2;
-		 * cities[4][0] = 2; cities[4][1] = 3; cities[5][0] = 5; cities[5][1] = 6;
+		 * int m = Integer.parseInt(firstMultipleInput[1]);
 		 * 
+		 * int k = Integer.parseInt(firstMultipleInput[2]);
 		 * 
-		 * //int[][] graph = DsAlgoUtils.makeGraph(3, cities);
+		 * List<String> centers = IntStream.range(0, n).mapToObj(i -> { try { return
+		 * bufferedReader.readLine(); } catch (IOException ex) { throw new
+		 * RuntimeException(ex); } }) .collect(Collectors.toList());
 		 * 
-		 * long minCost = roadsAndLibraries(9, 91, 84, cities);
-		 * System.out.println(minCost);
+		 * List<List<Integer>> roads = new ArrayList<>();
+		 * 
+		 * IntStream.range(0, m).forEach(i -> { try { roads.add(
+		 * Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+		 * .map(Integer::parseInt) .collect(Collectors.toList()) ); } catch (IOException
+		 * ex) { throw new RuntimeException(ex); } });
+		 * 
+		 * int res = shop(n, k, centers, roads);
+		 * 
+		 * System.out.println(res); System.out.println(); isCompleted = true; } } catch
+		 * (Exception e) { e.printStackTrace(); }
 		 */
+		 
 	}
-
-	// Complete the roadsAndLibraries function below.
-	static long roadsAndLibraries(int n, int c_lib, int c_road, int[][] cities) {
-		long totalCost = 0;
-		// List<List<Vertex>> connList = new ArrayList<>();
-		List<Set<Vertex>> connSetList = new ArrayList<>();
-		if (0 == cities.length || c_lib <= c_road) {
-			return (long) n * c_lib;
+	
+	public static int shop(int n, int k, List<List<Integer>> centers, List<List<Integer>> roads) {
+		int minTime = Integer.MAX_VALUE;
+		int[][] minTimeArr = new int[n][(int) Math.pow(2.0, Double.valueOf(String.valueOf(k)))];
+		int allFishTypesBits = 0;
+		
+		for (int i = 0; i < k; i++) {
+			allFishTypesBits = setKthBit(allFishTypesBits, i);
 		}
-
+		
+		for (int i = 0; i < minTimeArr.length; i++) {
+			for (int j = 0; j < minTimeArr[i].length; j++) {
+				minTimeArr[i][j] = Integer.MAX_VALUE;
+			}
+		}
+		
 		MyLinkedListGraph graph = new MyLinkedListGraph(true, false);
 
 		for (int i = 1; i <= n; i++) {
@@ -103,62 +264,114 @@ public class DsAlgoPractice {
 			graph.add(v);
 		}
 
-		for (int i = 0; i < cities.length; i++) {
-			int sourceNode = cities[i][0];
-			int destNode = cities[i][1];
+        for (int i = 0; i < roads.size(); i++) {
+			Integer sourceNode = roads.get(i).get(0);
+			Integer destNode = roads.get(i).get(1);
+			Integer edge = roads.get(i).get(2);
+			int fishTypesBits = 0;
+			int sourceFishTypesBits = 0;
+			int destFishTypesBits = 0;
+			List<Integer> sourceNodeFishTypes = centers.get(sourceNode - 1);
+			List<Integer> destNodeFishTypes = centers.get(destNode - 1);
+			
+			for (Integer bit : sourceNodeFishTypes) {
+				fishTypesBits = setKthBit(fishTypesBits, bit - 1);
+				sourceFishTypesBits = setKthBit(sourceFishTypesBits, bit - 1);
+			}
+			graph.getVertices().get(sourceNode - 1).get(0).setMaskedBits(sourceFishTypesBits);
+			
+			for (Integer bit : destNodeFishTypes) {
+				fishTypesBits = setKthBit(fishTypesBits, bit - 1);
+				destFishTypesBits = setKthBit(destFishTypesBits, bit - 1);
+			}
+			graph.getVertices().get(destNode - 1).get(0).setMaskedBits(destFishTypesBits);
+			
+			Vertex destVertex = new Vertex();
+			Vertex sourceVertex = new Vertex();
 
 			List<Vertex> sourceNodeList = graph.getVertices().get(sourceNode - 1);
-			Vertex destVertex = graph.getVertices().get(destNode - 1).get(0);
-
 			List<Vertex> destNodeList = graph.getVertices().get(destNode - 1);
-			Vertex sourceVertex = graph.getVertices().get(sourceNode - 1).get(0);
-
+			
+			destVertex.setName(String.valueOf(destNode));
+			destVertex.setEdge(edge);
+			destVertex.setParent(sourceNodeList.get(0));
+			destVertex.setDistance(Integer.MAX_VALUE);
+			destVertex.setMaskedBits(fishTypesBits);
+			destVertex.setMainVertexListIndex(destNodeList.get(0).getArrayIndex());
+			
 			sourceNodeList.add(destVertex);
+			destVertex.setArrayIndex(sourceNodeList.size() - 1);
+			
+			sourceVertex.setName(String.valueOf(sourceNode));
+			sourceVertex.setEdge(edge);
+			sourceVertex.setParent(destNodeList.get(0));
+			sourceVertex.setDistance(Integer.MAX_VALUE);
+			sourceVertex.setMaskedBits(fishTypesBits);
+			sourceVertex.setMainVertexListIndex(sourceNodeList.get(0).getArrayIndex());
+			
 			destNodeList.add(sourceVertex);
-		}
-
-		Stack<Vertex> stack = new Stack<>();
-
-		for (List<Vertex> temp : graph.getVertices()) {
-			if (temp.size() <= 1) {
-				continue;
-			}
-			if (!temp.get(0).isVisited()) {
-				stack.push(temp.get(0));
-				Set<Vertex> connNodes = new HashSet<>();
-				connSetList.add(connNodes);
-				while (!stack.isEmpty()) {
-					Vertex vertex = stack.pop();
-
-					if (!vertex.isVisited()) {
-						connSetList.get(connSetList.size() - 1).add(vertex);
-						vertex.setVisited(true);
-						List<Vertex> adjacentVertices = graph.getVertices().get(vertex.getArrayIndex());
-
-						for (int i = 1; i < adjacentVertices.size(); i++) {
-							if (!adjacentVertices.get(i).isVisited()) {
-								stack.push(adjacentVertices.get(i));
-							}
+			sourceVertex.setArrayIndex(destNodeList.size() - 1);
+        }
+        
+        Queue<Vertex> q = new LinkedList<>();
+		
+		q.add(graph.getVertices().get(0).get(0));
+		graph.getVertices().get(0).get(0).setDistance(0);
+		
+		while (!q.isEmpty()) {
+			Vertex vertex = q.poll();
+			
+			if (!vertex.isVisited()) {
+				vertex.setVisited(true);
+				List<Vertex> adjacentVertices = graph.getVertices().get(vertex.getArrayIndex());
+				
+				for (int i = 1; i < adjacentVertices.size(); i++) {
+					Vertex dest = adjacentVertices.get(i);
+					if (!dest.isVisited()) {
+						int time = vertex.getDistance() + dest.getEdge();
+						int fishTypesBits = vertex.getMaskedBits() | dest.getMaskedBits();
+						int currentMinTime = minTimeArr[dest.getMainVertexListIndex()][fishTypesBits];
+						
+						if (time < currentMinTime) {
+							minTimeArr[dest.getMainVertexListIndex()][fishTypesBits] = time;
+							dest.setDistance(time);
+							dest.setMaskedBits(fishTypesBits);
+							graph.getVertices().get(dest.getMainVertexListIndex()).get(0).setMaskedBits(fishTypesBits);
+							graph.getVertices().get(dest.getMainVertexListIndex()).get(0).setDistance(time);
+							q.add(graph.getVertices().get(dest.getMainVertexListIndex()).get(0));
 						}
 					}
 				}
 			}
 		}
-
-		long totalNodeCount = 0;
-		for (int i = 0; i < connSetList.size(); i++) {
-			int connCount = connSetList.get(i).size();
-			System.out.println("Set size:" + connCount);
-			long tempCost = c_lib + ((connCount - 1) * c_road);
-			totalCost += tempCost;
-			totalNodeCount += connCount;
+		
+		for (int i = 0; i < minTimeArr[k - 1].length; i++) {
+			for (int j = i + 1; j < minTimeArr[k - 1].length; j++) {
+				int totalFishTypes = i | j;
+				
+				if (allFishTypesBits == totalFishTypes) {
+					int time1 = minTimeArr[k - 1][i];
+					int time2 = minTimeArr[k - 1][j];
+					int maxTime = 0;
+					
+					if (time1 > time2) {
+						maxTime = time1;
+					} else {
+						maxTime = time2;
+					}
+					if (maxTime < minTime) {
+						minTime = maxTime;
+					}
+				}
+			}
 		}
-
-		if (n > totalNodeCount) {
-			totalCost += (n - totalNodeCount) * c_lib;
-		}
-
-		return totalCost;
+		
+		return minTime;
+	}
+	
+	static int setKthBit(int n, int k) { 
+	    // kth bit of n is being set by this operation 
+	    return ((1 << k) | n); 
 	}
 
 }
